64e8777e865aaceed676fa3643240553625c55fc
heon-kim <vndtka123@gmail.com>
2022-02-20 15:53:40 +0900

    Update weather.js & Add .gitignore

--- src/weather.js
+++ src/weather.js
@@ -1,18 +1,20 @@
-const API_key = "";
-
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&lang=kr`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const city = document.querySelector("#weather__city");
       const temperature = document.querySelector("#weather__temperature");
-      const climate = document.querySelector("weather__climate");
+      const description = document.querySelector("#weather__description");
+      const tempInfo = document.querySelector("#weather__temperature--info");
       city.innerText = `${data.name}`;
-      temperature.innerText = `${data.main.temp}℃`;
-      climate.innerText = `${data.weather[0].main}`;
+      temperature.innerText = `${Math.floor(data.main.temp)}℃`;
+      description.innerText = `${data.weather[0].description}`;
+      tempInfo.innerText = `최고: ${Math.floor(
+        data.main.temp_max
+      )} 최저: ${Math.floor(data.main.temp_min)}`;
     });
 }
 
